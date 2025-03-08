d88f21900964719746996df0e9118c05e0369c11
Carolyne Lira <carol017lira@gmail.com>
2025-03-08 17:07:57 -0300

    feat: Cria requisição POST para agendamento

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,4 +1,7 @@
 import dayjs from "dayjs"
+
+import { scheduleNew } from "../../services/schedule-new.js"
+
 const form = document.querySelector("form")
 const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
@@ -9,7 +12,7 @@ selectedDate.value = inputToday
 
 selectedDate.min = inputToday
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
   event.preventDefault()
 
   try {
@@ -31,7 +34,7 @@ form.onsubmit = (event) => {
 
     const id = new Date().getTime()
 
-    console.log({
+    await scheduleNew({
       id,
       name,
       when
